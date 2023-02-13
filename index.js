224dae0dc06bcd47584b6fc6ecca5e8ce018d081
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-13 18:41:32 -0500

    abstract grid size

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
   Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -35,3 +36,19 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+//Maze Generation
+
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+console.log(grid);
