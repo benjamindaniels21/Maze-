4d9b782d6e3ca0867fb6cf87acf51d51f655c1f1
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-13 16:50:11 -0500

    adjust wall sizes dynamically

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
   Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -13,7 +13,7 @@ const render = Render.create({
   options: {
     width: width,
     height: height,
-    wireframes: false,
+    wireframes: true,
   },
 });
 
@@ -28,30 +28,10 @@ World.add(
 
 //Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 
 World.add(world, walls);
-
-//Random Shapes
-
-for (let i = 0; i < 50; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-        render: {
-          fillStyle: "red",
-        },
-      })
-    );
-  }
-}
